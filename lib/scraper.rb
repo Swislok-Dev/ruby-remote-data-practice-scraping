c81ec060c95970936ecbf7a6227d3cec8c4ad805
cmarker <cmarker4@gmail.com>
2020-11-26 11:28:39 -0600

    commit tree 3842b2b92ef11a3b4e85d437b9781b76a448a199

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+
+# doc = Nokogiri::HTML(html)  # puts will print out the HTML for webpage 'doc'
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".headline-26OIBN").text
+
